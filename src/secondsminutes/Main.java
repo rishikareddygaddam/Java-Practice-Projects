f1ac9cf87c460d019b1be3782f7821aca5562358
rishikareddygaddam <rishika.reddy.g5@gmail.com>
2020-05-29 17:39:49 -0400

    Seconds to minutes to hours main

--- src/secondsminutes/Main.java
+++ src/secondsminutes/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-        System.out.println(SecondsToMinutes.getDurationString(65,45));
+        System.out.println(SecondsToMinutes.getDurationString(65,35));
         System.out.println(SecondsToMinutes.getDurationString(3601));
         System.out.println(SecondsToMinutes.getDurationString(-41));
         System.out.println(SecondsToMinutes.getDurationString(65,9));
